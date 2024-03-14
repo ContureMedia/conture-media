dfb4a3bd6a97d2fe9a537048f3632d7b1db05b88
riteshk-007 <codeshorts007@gmail.com>
2024-03-14 11:35:29 +0530

    Update Navbar link to "/more"

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -135,7 +135,7 @@ const Navbar = () => {
                       </ul>
                       <div className="py-1">
                         <Link
-                          href="/"
+                          href="/more"
                           onClick={() => setIsOpen(false)}
                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
