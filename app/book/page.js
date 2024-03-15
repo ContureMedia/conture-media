e0c601212ba2cb07dc61c1453daf429bc9560929
riteshk-007 <codeshorts007@gmail.com>
2024-03-15 11:31:09 +0530

    Update page.js and next.config.mjs

--- app/book/page.js
+++ app/book/page.js
@@ -14,15 +14,19 @@ const EBook = () => {
         background: "radial-gradient(circle, #111 80%, black 100%)",
       }}
     >
-      <h1 className="text-4xl md:text-6xl font-bold mb-5 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-        Free E-Book
+      <h1 className="text-4xl md:text-6xl uppercase font-extrabold mb-5 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+        Grab your Free E-Book
       </h1>
       <p className="text-center text-2xl font-semibold md:text-3xl mb-5">
         Global Service Provider{" "}
       </p>
-      <p className="text-center text-xl md:text-2xl mb-5">
-        Grab your free e-book now and unlock the secrets to making your content
-        and videos go viral in the marketing world!
+      <p className="text-center text-xl md:text-2xl font-bold mb-5 capitalize from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">
+        {/* Grab your free e-book now and */}
+        <q>
+          {" "}
+          unlock the secrets to making your content and videos go viral in the
+          marketing world !
+        </q>
       </p>
 
       <div className="flex flex-col md:flex-row items-center justify-evenly w-full">
