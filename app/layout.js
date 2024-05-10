c3a4d0cfa73dcaedc25a895261422100f8cfada8
Riteshk_007 <codeshorts007@gmail.com>
2024-05-10 18:18:46 +0530

    chore: Refactor metadata in layout.js for improved SEO and branding

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,79 @@ import Footer from "./components/Footer";
 import SocialLinks from "./components/SocialLinks";
 
 const inter = Inter({ subsets: ["latin"] });
-export const metadata = {
-  title:
-    "Conture Media - Video Editing, Social Media Marketing, and Content Creation Services | Conture Media",
-  description:
-    "Conture Media is a digital marketing agency that specializes in video editing, social media marketing, and content creation services. We help businesses grow their online presence and reach their target audience through engaging video content and social media marketing strategies.",
-};
 
+export const metadata = {
+  title: "Conture Media - Premium Content Creation, Video Editing & Marketing",
+  description:
+    "Elevate your brand with Conture Medias expert content creation, video production, marketing services & community engagement. Crafted by industry leaders for unparalleled quality and results.",
+  keywords: [
+    "Conture Media",
+    "Content Creation",
+    "Video Editing",
+    "Marketing",
+    "Digital Marketing",
+    "Social Media Marketing",
+    "Community",
+    "New York",
+  ],
+  openGraph: {
+    title:
+      "Conture Media - Premium Content Creation, Video Editing & Marketing",
+    description:
+      "Elevate your brand with Conture Medias expert content creation, video production, marketing services & community engagement. Crafted by industry leaders for unparalleled quality and results.",
+    images: [
+      {
+        url: "https://www.conturemedia.com/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Conture Media - Premium Content Creation, Video Editing & Marketing",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  other: {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Conture Media",
+    url: "https://www.conturemedia.com",
+    logo: "https://www.conturemedia.com/logo.png",
+    sameAs: [
+      "https://www.instagram.com/conture_media",
+      "https://www.linkedin.com/company/conture-media/",
+    ],
+    contactPoint: {
+      "@type": "ContactPoint",
+      email: "Contureai1@gmail.com",
+    },
+    founders: [
+      {
+        "@type": "Person",
+        name: "Manthan Gupta",
+      },
+    ],
+  },
+  metadataBase: "https://www.conturemedia.com/",
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+  twitter: {
+    site: "@conture_media",
+    cardType: "summary_large_image",
+  },
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
